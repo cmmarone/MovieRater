e976ff56539b0307b7d4fff93f6d0ee156f8bda2
cmmarone <cmmarone@gmail.com>
2021-04-15 10:33:56 -0400

    collinDataTier

---
+++ MovieRater.Data/Rating.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MovieRater.Data
+{
+    public class Rating
+    {
+        [Key]
+        public int RatingId { get; set; }
+
+        [Required, Range(0, 5)]
+        public double StarRating { get; set; }
+
+        [ForeignKey(nameof(Movie))]
+        public int MovieId { get; set; }
+        public virtual Movie Movie { get; set; }
+
+        [ForeignKey(nameof(TVShow))]
+        public int TVShowId { get; set; }
+        public virtual TVShow TVShow { get; set; }
+    }
+}
