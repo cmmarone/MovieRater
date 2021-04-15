909a1ec153008a1ec0eebaa8f4ee49af101a81eb
daniellejackson <weathers.danielle@ymail.com>
2021-04-15 11:02:50 -0400

    CorrectedfirstpushDani

--- MovieRater.Data/Movie.cs
+++ MovieRater.Data/Movie.cs
@@ -1,12 +1,37 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace MovieRater.Data
 {
-    class Movie
+    public class Movie
     {
+        [Key]
+        public int MovieId { get; set; }
+
+        [Required]
+        public string Title { get; set; }
+
+        public virtual List<Rating> Ratings { get; set; } = new List<Rating>();
+
+        public double Rating
+        {
+            get
+            {
+                double totalAverageRating = 0;
+               
+                foreach (var rating in Ratings)
+                {
+                    totalAverageRating += rating.StarRating;
+                }
+                
+                return Ratings.Count > 0
+                    ? Math.Round(totalAverageRating / Ratings.Count, 1) 
+                      : 0; 
+            }
+        }
     }
 }
