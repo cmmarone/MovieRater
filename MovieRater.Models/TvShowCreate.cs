2da697eb9cc72b9a5e4f31bdb3aca2df3d250b87
vscott1989 <vscott1811@gmail.com>
2021-04-15 14:08:33 -0400

    UpdateModels

---
+++ MovieRater.Models/TvShowCreate.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MovieRater.Models
+{
+    class TvShowCreate
+    {
+        [Required]
+        public string Title { get; set; }
+        public int Seasons { get; set; }
+        public int Episode { get; set; }
+
+    }
+}
